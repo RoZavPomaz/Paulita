5f6d28a7500290739d42b32d15851efc8971b883
Roberto Zavala <robertozavala666@gmail.com>
2026-06-18 00:04:32 -0500

    avances general

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -1,12 +1,12 @@
 import { FaRegCalendarAlt, FaRegHeart, FaRegStar } from "react-icons/fa";
-
+import MotoImg from "../assets/images/MotoImage.png";
 const Page2 = () => {
   return (
-    <div className=" flex flex-col h-screen justify-around items-center text-white">
+    <div className=" flex flex-col h-screen items-center text-white">
       <section>
-        <div className="text-center flex flex-col gap-2">
+        <div className="text-center flex flex-col gap-2 mt-5">
           <p>Un año de </p>
-          <h2>Momentos</h2>
+          <h2 className="font-birthstone text-5xl">Momentos</h2>
           <div className="flex  gap-3 items-center justify-center">
             <hr className="w-10" />
             <FaRegHeart />
@@ -16,31 +16,32 @@ const Page2 = () => {
 
         <div className="flex flex-col gap-3 mt-6">
           <div className="flex items-center gap-3">
-            <div className="rounded-lg bg-white/80 w-fit p-3">
+            <div className="rounded-lg bg-white/70 w-fit p-4">
               <FaRegCalendarAlt size={30} color="gray" />
             </div>
             <p className="font-medium">365 días</p>
           </div>
           <div className="flex items-center gap-3">
-            <div className="rounded-lg bg-white/80 w-fit p-3">
+            <div className="rounded-lg bg-white/70 w-fit p-4">
               <FaRegHeart size={30} />
             </div>
             <p className="font-medium">12 meses</p>
           </div>
           <div className="flex items-center gap-3">
-            <div className="rounded-lg bg-white/80 w-fit p-3">
+            <div className="rounded-lg bg-white/70 w-fit p-4">
               <FaRegStar size={30} />
             </div>
             <p className="font-medium">Muchos recuerdos</p>
           </div>
         </div>
+        <img src={MotoImg} alt="" className="size-45 mt-5" />
       </section>
-      <div className="text-center flex flex-col gap-2">
+      <div className="text-center flex flex-col gap-2 mt-10">
         <p>Cada día</p>
         <p>ha tenido algo</p>
-        <h2>especial</h2>
+        <h2 className="font-birthstone text-5xl">especial</h2>
         <div className="flex justify-center">
-          <FaRegHeart />
+          <FaRegHeart color="gold" />
         </div>
       </div>
     </div>
--- src/components/Page3.jsx
+++ src/components/Page3.jsx
@@ -1,9 +1,16 @@
 import { CiCamera, CiFaceSmile, CiHeart } from "react-icons/ci";
+import { FaRegHeart } from "react-icons/fa";
 import { IoSunnyOutline } from "react-icons/io5";
 
 const Page3 = () => {
   return (
     <div className="h-screen">
+      <div className="flex flex-col items-center text-white">
+        <p className="mb-2">Entre tantos días,</p>
+        <p>hay cosas que me</p>
+        <h2 className="font-birthstone text-5xl">encantan de ti</h2>
+        <FaRegHeart color="gold" className="mx-auto mb-5 mt-3" />
+      </div>
       <div className="flex flex-col gap-5 items-center">
         <div className="flex items-center justify-center gap-5 px-5 py-8 w-[70%] bg-white/40 rounded-3xl">
           <CiFaceSmile size={55} color="gold" />
@@ -17,27 +24,28 @@ const Page3 = () => {
         <div className="flex items-center justify-center gap-5 px-5 py-8 w-[70%] bg-white/40 rounded-3xl">
           <CiCamera size={55} color="gold" />
           <div>
-            <h2 className="font-medium">Tu sonrisa</h2>
-            <p className="font-light">Que ilumina</p>
-            <p className="font-light">cualquier lugar.</p>
+            <h2 className="font-medium">Tu mirada</h2>
+            <p className="font-light">Que ve la belleza</p>
+            <p className="font-light">en los detalles.</p>
           </div>
         </div>
         <div className="flex items-center justify-center gap-5 px-5 py-8 w-[70%] bg-white/40 rounded-3xl">
           <CiHeart size={55} color="gold" />
 
           <div>
-            <h2 className="font-medium">Tu sonrisa</h2>
-            <p className="font-light">Que ilumina</p>
-            <p className="font-light">cualquier lugar.</p>
+            <h2 className="font-medium">Tu forma de ser</h2>
+            <p className="font-light">Unica, auténtica</p>
+            <p className="font-light">y maravillosa.</p>
           </div>
         </div>
         <div className="flex items-center justify-center gap-5 px-5 py-8 w-[70%] bg-white/40 rounded-3xl">
           <IoSunnyOutline size={55} color="gold" />
 
           <div>
-            <h2 className="font-medium">Tu sonrisa</h2>
-            <p className="font-light">Que ilumina</p>
-            <p className="font-light">cualquier lugar.</p>
+            <h2 className="font-medium">Tu amor por </h2>
+            <h2 className="font-medium">el cielo</h2>
+            <p className="font-light">Como si cada uno</p>
+            <p className="font-light">fuera magico.</p>
           </div>
         </div>
       </div>
